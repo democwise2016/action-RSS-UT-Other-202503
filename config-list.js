1f92b773734d05f43ed55487b742e75b4aec021b
Pulipuli Chen <pulipuli.chen@gmail.com>
2025-05-18 00:50:27 +0800

    Update config-list.js

--- config-list.js
+++ config-list.js
@@ -11,14 +11,14 @@ let feedList = [
   //     ItemFiltersPreset.between1minTo10Min,
   //   ] // 霸道總裁攀岩開會、網紅天天搞小團體...《絕世網紅》劇情超浮誇？《 志祺今天不讀稿 》EP010｜志祺七七
   // },
-  {
-    title: '番茄没有酱聊AI繪圖',
-    feedID: 'lsm0315',
-    homepageURL: 'https://www.youtube.com/channel/UCoYbSADuNZvm99BLxqtImYQ',
-    itemFilters: [
-      ItemFiltersPreset.between3minTo30Min,
-    ] // 霸道總裁攀岩開會、網紅天天搞小團體...《絕世網紅》劇情超浮誇？《 志祺今天不讀稿 》EP010｜志祺七七
-  },
+  // {
+  //   title: '番茄没有酱聊AI繪圖',
+  //   feedID: 'lsm0315',
+  //   homepageURL: 'https://www.youtube.com/channel/UCoYbSADuNZvm99BLxqtImYQ',
+  //   itemFilters: [
+  //     ItemFiltersPreset.between3minTo30Min,
+  //   ] // 霸道總裁攀岩開會、網紅天天搞小團體...《絕世網紅》劇情超浮誇？《 志祺今天不讀稿 》EP010｜志祺七七
+  // },
   {
     title: '敖厂长',
     feedID: 'hawkaoaoful',
